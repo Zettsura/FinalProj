67ea5b1875df09e53eabe7352030083303939da6
MisterSuav3 <202311582@fit.edu.ph>
2024-11-25 19:51:48 +0800

    Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,18 @@
+public class Vehicle {
+    public long vehicleId;
+    public String carModel;
+    public String carType;
+    public String color;
+    public String fuelType;
+    public String transType;
+    public int modelId;
+    public int storageLim;
+    public int passLim;
+    public float mileageLim;
+    public float basePrice;
+    public boolean isRented;
+    public boolean hasTintedWindows;
+    public boolean hasFuel;
+}
+
+
